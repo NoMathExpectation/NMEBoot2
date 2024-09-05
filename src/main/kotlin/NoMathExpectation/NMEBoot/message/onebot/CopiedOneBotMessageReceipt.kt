2f26ca7a35582d599d4c023296e3d3089e70ebdd
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-09-05 19:16:03 +0800

    Fixed CopiedOneBotMessageReceipt not implementing SingleMessageReceipt.

--- src/main/kotlin/NoMathExpectation/NMEBoot/message/onebot/CopiedOneBotMessageReceipt.kt
+++ src/main/kotlin/NoMathExpectation/NMEBoot/message/onebot/CopiedOneBotMessageReceipt.kt
@@ -6,12 +6,15 @@ import love.forte.simbot.common.id.ID
 import love.forte.simbot.component.onebot.v11.core.api.DeleteMsgApi
 import love.forte.simbot.component.onebot.v11.core.bot.OneBotBot
 import love.forte.simbot.component.onebot.v11.message.OneBotMessageReceipt
+import love.forte.simbot.message.SingleMessageReceipt
 
 // copied from sdk
 class CopiedOneBotMessageReceipt(
     override val messageId: ID,
     private val bot: OneBotBot,
-) : OneBotMessageReceipt {
+) : SingleMessageReceipt(), OneBotMessageReceipt {
+    override val id = messageId
+
     override suspend fun delete(vararg options: DeleteOption) {
         kotlin.runCatching {
             bot.executeData(DeleteMsgApi.create(messageId))
