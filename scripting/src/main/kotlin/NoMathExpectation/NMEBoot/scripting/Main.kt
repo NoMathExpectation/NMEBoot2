1076cb4acf52700e7fcbc6ebfbd96cbe075c4302
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2025-08-01 20:18:43 +0800

    Fixed scripting not using logger.

--- scripting/src/main/kotlin/NoMathExpectation/NMEBoot/scripting/Main.kt
+++ scripting/src/main/kotlin/NoMathExpectation/NMEBoot/scripting/Main.kt
@@ -1,5 +1,6 @@
 package NoMathExpectation.NMEBoot.scripting
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
@@ -9,11 +10,13 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
+private val logger = KotlinLogging.logger { }
+
 fun main() {
-    println("Preheating eval service...")
+    logger.info { "Preheating eval service..." }
     val eval = "print(\"Hello, world!\")".toSimpleEval()
     eval()
-    println(eval.output)
+    logger.info { eval.output }
 
     embeddedServer(CIO, port = 8080) {
         install(ContentNegotiation) {
